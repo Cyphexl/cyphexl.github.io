6a2826677655a5eee118e7a945983d6f4d3a08b5
Tzingtao Chow <Cyphexl@outlook.com>
2021-02-13 16:46:27 +0800

    Site updated: 2021-02-13 16:46:22

---
+++ js/scrollbar.js
@@ -0,0 +1,17 @@
+if (!is_touch_device()) {
+    // Alternative lib: SimpleBar
+    // Array.prototype.forEach.call(document.querySelectorAll('.highlight table'), el => new SimpleBar);
+
+    // OverlayScrollBars
+    document.addEventListener("DOMContentLoaded", function () {
+        //The first argument are the elements to which the plugin shall be initialized
+        //The second argument has to be at least a empty object or a object with your desired options
+        OverlayScrollbars(document.querySelectorAll('.highlight table, .katex-display'), {
+            className: "os-theme-minimal-dark",
+            scrollbars: {
+                autoHide: "leave",
+                autoHideDelay: 10
+            }
+        });
+    });
+}
